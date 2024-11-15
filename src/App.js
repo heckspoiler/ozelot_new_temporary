15a01803efa23194fba520efb400a60f2bc45d31
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-11-15 13:51:20 +0100

    first push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Scene from './components/scene/Scene';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <body>
+        <Scene />
+        <h1>OZELOT STUDIOS</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
