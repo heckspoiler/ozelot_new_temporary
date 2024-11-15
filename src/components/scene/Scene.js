d7c5c6962ade72ad0ee11852712fc50a6b93856e
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-11-15 13:53:00 +0100

    changes

--- src/components/scene/Scene.js
+++ src/components/scene/Scene.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
+import styles from './Scene.module.css';
+
 export default function Scene() {
-  return (
-    <div>
-      <h1>Scene</h1>
-    </div>
-  );
+  return <div className={styles.main}></div>;
 }
