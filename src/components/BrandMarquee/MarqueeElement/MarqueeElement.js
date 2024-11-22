5c43333666564b1153ce668408bb4e30f04bb1a4
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-11-22 17:35:22 +0100

    changes

--- src/components/BrandMarquee/MarqueeElement/MarqueeElement.js
+++ src/components/BrandMarquee/MarqueeElement/MarqueeElement.js
@@ -5,13 +5,8 @@ import styles from './MarqueeElement.module.css';
 export default function MarqueeElement({ src, alt, brandname, url, index }) {
   return (
     <div className={styles.container} key={`${brandname} ${index}`}>
-      <div className={styles.textContainer}>
-        <p>{brandname}</p>
-      </div>
-
       <a href={url ? url : ''} target="_blank" rel="noreferrer">
         <div className={styles.imageContainer}>
-          <div className={styles.overlay}></div>
           <img src={src} alt={alt} />
         </div>
       </a>
