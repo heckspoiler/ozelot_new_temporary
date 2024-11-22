f7666657b2aefba7ff0b9050b59fdc7bff9b511f
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2024-11-22 17:24:57 +0100

    change

--- src/components/BrandMarquee/BrandMarquee.js
+++ src/components/BrandMarquee/BrandMarquee.js
@@ -12,12 +12,12 @@ console.log(brandArray[0].path);
 export default function BrandMarquee() {
   return (
     <section className={styles.marqueeSection}>
-      <Marquee gradient={false} speed={30}>
+      <Marquee gradient={false} speed={30} autoFill pauseOnHover>
         {brandArray.map((brand, index) => (
           <MarqueeElement
             src={brand.src}
             alt={brand.alt}
-            brandname={brand.brandname}
+            brandname={brand.name}
             url={brand.url}
             path={brand.path}
             index={index}
--- src/components/BrandMarquee/MarqueeElement/MarqueeElement.js
+++ src/components/BrandMarquee/MarqueeElement/MarqueeElement.js
@@ -5,6 +5,10 @@ import styles from './MarqueeElement.module.css';
 export default function MarqueeElement({ src, alt, brandname, url, index }) {
   return (
     <div className={styles.container} key={`${brandname} ${index}`}>
+      <div className={styles.textContainer}>
+        <p>{brandname}</p>
+      </div>
+
       <a href={url ? url : ''} target="_blank" rel="noreferrer">
         <div className={styles.imageContainer}>
           <div className={styles.overlay}></div>
